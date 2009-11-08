2080c9314c8eda9f65c90fc872db172c6a2fdd49
Ben Hutchings <ben@decadent.org.uk>
2009-11-08 01:44:55 +0000

    Fix libavcodec backward-compatibility code
    Guard against multiple inclusion now that we have our own
    (conditional) definitions which must not be repeated.
    
    Define av_init_packet() for libavcodec < 52.26.0.
    
    Define AVPacket for libavcodec < 52.23.0.
    
    Define AV_VERSION_INT if necessary.

--- src/avcodec_wrap.h
+++ src/avcodec_wrap.h
@@ -1,6 +1,9 @@
 // Copyright 2009 Ben Hutchings.
 // See the file "COPYING" for licence details.
 
+#ifndef AVCODEC_WRAP_H
+#define AVCODEC_WRAP_H
+
 // Some versions of ffmpeg define an ABS macro, which glib also does.
 // The two definitions are equivalent but the duplicate definitions
 // provoke a warning.
@@ -13,7 +16,35 @@ extern "C" {
 
 #include <avcodec.h>
 
+#ifdef __cplusplus
+}
+#endif
+
+#undef ABS
+
+#ifndef AV_VERSION_INT
+#define AV_VERSION_INT(a, b, c) (a<<16 | b<<8 | c)
+#endif
+
 #if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(52, 26, 0)
+
+#include <stdint.h>
+#include <string.h>
+
+#if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(52, 23, 0)
+/* avcodec.h doesn't use AVPacket at all, so provide our own minimal
+ * definition. */
+typedef struct {
+    uint8_t *data;
+    int size;
+} AVPacket;
+#endif
+
+static inline void av_init_packet(AVPacket *avpkt)
+{
+    memset(avpkt, 0, sizeof(*avpkt));
+}
+
 static inline int
 avcodec_decode_video2(AVCodecContext *avctx, AVFrame *picture,
 		      int *got_picture_ptr, AVPacket *avpkt)
@@ -21,10 +52,7 @@ avcodec_decode_video2(AVCodecContext *avctx, AVFrame *picture,
     return avcodec_decode_video(avctx, picture, got_picture_ptr,
 				avpkt->data, avpkt->size);
 }
-#endif
 
-#ifdef __cplusplus
-}
-#endif
+#endif /* < 52.26.0 */
 
-#undef ABS
+#endif /* AVCODEC_WRAP_H */
