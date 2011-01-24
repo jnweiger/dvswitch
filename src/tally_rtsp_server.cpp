6a3f61f9e85a153734d3040513f7187f2c35b933
Wouter Verhelst <w@uter.be>
2011-01-24 20:38:17 +0100

    Change case to be compatible with pre-rtsp dvsource implementation

--- src/tally_rtsp_server.cpp
+++ src/tally_rtsp_server.cpp
@@ -18,7 +18,7 @@ tally_rtsp_server::tally_rtsp_server(int pipefd, UsageEnvironment& env, int ourS
     : RTSPServer(env, ourSocket, ourPort, authDatabase, reclamationTestSeconds),
       pipefd_(pipefd), tally_state_(TALLY_OFF)
 {
-    write(pipefd_, "TALLY: OFF\n", 11);
+    write(pipefd_, "TALLY: off\n", 11);
 }
 
 tally_rtsp_server::~tally_rtsp_server()
@@ -64,15 +64,15 @@ tally_rtsp_server::RTSPClientSession::handleCmd_SET_PARAMETER(ServerMediaSubsess
     if (!tallyloc) {
     	snprintf((char*)fResponseBuffer, sizeof(fResponseBuffer), "RTSP/1.0 200 OK\r\nCSeq: %s\r\n%sSession: %08X\r\n\r\n", cseq, dateHeader(), fOurSessionId);
     }
-    if (strstr(tallyloc, "ON"))
+    if (strstr(tallyloc, "on"))
     {
         newstate = TALLY_ON;
     }
-    else if (strstr(tallyloc, "OFF"))
+    else if (strstr(tallyloc, "off"))
     {
         newstate = TALLY_OFF;
     }
-    else if (strstr(tallyloc, "CUE"))
+    else if (strstr(tallyloc, "cue"))
     {
         newstate = TALLY_CUE;
     }
