2a926884786cc9518d7c5014ea5479e368d48adf
Wouter Verhelst <w@uter.be>
2011-02-01 12:59:36 +0100

    Implement tally lights in the RTSP server
    Known issues:
    - Writing to the script is not done nonblocking currently. We'll just
      have to make sure that the script runs quickly enough (doesn't seem to
      be a serious problem in my testing, anyway)
    - Server implements cue lights as well, but client doesn't. I didn't
      want to complicate the interface, since we won't need it all that
      often anyway.
    - The tally_rtsp_server class takes a filedescriptor, not the script
      name. This is because I thought it really shouldn't care about where
      the output goes, and that makes it slightly more flexible, but this
      might turn out not to be all that good of an idea in the end. Dunno.

---
+++ src/tally_rtsp_server.hpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2011 Wouter Verhelst
+ * See the file "COPYING" for license details
+ */
+#ifndef INC_TALLY_RTSP_SERVER_HPP
+#define INC_TALLY_RTSP_SERVER_HPP
+
+#ifndef _RTSP_SERVER_HH
+#include "RTSPServer.hh"
+#endif
+
+class tally_rtsp_server: public RTSPServer
+{
+public:
+    static tally_rtsp_server* createNew(int pipefd, bool verbose, UsageEnvironment& env, Port ourPort=554, UserAuthenticationDatabase* authDatabase=NULL, unsigned reclamationTestSeconds=65);
+protected:
+    tally_rtsp_server(int pipefd, bool verbose, UsageEnvironment& env, int ourSocket, Port ourPort, UserAuthenticationDatabase* authDatabase, unsigned reclamationTestSeconds);
+    virtual ~tally_rtsp_server();
+    virtual RTSPServer::RTSPClientSession* createNewClientSession(unsigned sessionId, int clientSocket, struct sockaddr_in clientAddr);
+private:
+    int pipefd_;
+    enum tally {
+        TALLY_ON,
+        TALLY_OFF,
+        TALLY_CUE
+    } tally_state_;
+    bool verbose_;
+    class RTSPClientSession: public RTSPServer::RTSPClientSession
+    {
+    public:
+        RTSPClientSession(tally_rtsp_server& ourServer, unsigned sessionId, int clientSocket, struct sockaddr_in clientAddr);
+	virtual ~RTSPClientSession();
+    protected:
+	virtual void handleCmd_SET_PARAMETER(ServerMediaSubsession* subsession, char const* cseq, char const* fullRequestStr);
+    private:
+        tally_rtsp_server& server_;
+    };
+};
+
+#endif
