95838c05470adf363fee02af7fb791df075255f1
Juergen Weigert <jw@suse.de>
2014-04-08 21:41:18 +0200

    The title_safe_frame now defaults to off. It is just a waste of time to explain to the volunteers, that they should ignore it.

--- src/mixer_window.cpp
+++ src/mixer_window.cpp
@@ -145,7 +145,7 @@ mixer_window::mixer_window(mixer & mixer, connector & connector)
     safe_area_menu_item_.signal_toggled().connect(
 	sigc::mem_fun(this, &mixer_window::toggle_safe_area_display));
     safe_area_menu_item_.show();
-    safe_area_menu_item_.set_active(true);
+    safe_area_menu_item_.set_active(false);
     fullscreen_menu_item_.signal_toggled().connect(
     	sigc::mem_fun(this, &mixer_window::toggle_fullscreen));
     fullscreen_menu_item_.show();
